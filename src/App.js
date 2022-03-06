bdd1a1859b5bb5f53ec2cec6b888a8fd56381162
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2022-03-07 00:31:14 +0100

    basic dummy api calls are working, user, and device table are present.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 // in src/App.js
 import * as React from "react";
-import { Admin, Resource, ListGuesser } from 'react-admin';
+import { Admin, Resource } from 'react-admin';
+import {PostCreate, PostList, PostEdit} from './posts';
+import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
+import PostIcon from '@material-ui/icons/Book';
+import UserIcon from '@material-ui/icons/Group';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
     <Admin dataProvider={dataProvider} >
-        <Resource name="users" list={ListGuesser} />
+        <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
+        <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>
 );
 
