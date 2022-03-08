d0c265e3b8c8af61e4e644f9817728b4452b2cdc
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2022-03-08 21:55:34 +0100

    added custom DataProvider for own API

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import { Admin, Resource } from 'react-admin';
 import {PostCreate, PostList, PostEdit} from './posts';
 import { UserList } from './users';
 import jsonServerProvider from 'ra-data-json-server';
+import DataProvider from "./DataProvider";
 import Dashboard from "./Dashboard";
 import authProvider from "./authProvider";
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 
-const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
+//const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 const App = () => (
-    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider} dataProvider={dataProvider} >
+    <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={DataProvider} >
         <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon}/>
         <Resource name="users" list={UserList} icon={UserIcon}/>
     </Admin>
