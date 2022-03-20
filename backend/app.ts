fe0bd0c899ff1963a2677170e08086791f3bd117
Bertalan Lovas <lovas>
2022-03-20 21:26:34 +0100

    Cors added

--- backend/app.ts
+++ backend/app.ts
@@ -1,5 +1,7 @@
 import { Application, Context } from 'https://deno.land/x/oak/mod.ts'
 import router from './routes/routes.ts';
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+
 
 const app = new Application();
 
@@ -9,5 +11,7 @@ app.use(router.allowedMethods())
 app.use((ctx: Context) => {
   ctx.response.body = "is dis even working? deno lol";
 });
+
+app.use(oakCors()); // Enable CORS for All Routes
 
 await app.listen({ port: 8000 });
