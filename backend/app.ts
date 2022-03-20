4d93888ceb5bcfea1746c81a5a1fec3d1115e70e
Bertalan Lovas <lovas>
2022-03-21 00:45:34 +0100

    done

--- backend/app.ts
+++ backend/app.ts
@@ -5,11 +5,10 @@ import router from './routes/routes.ts';
 const app = new Application();
 app.use(
     oakCors({
-      origin: /^.+localhost:(1234|3000)$/,
-      optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+      origin: /^.+localhost:(3000|8000)$/,
+      optionsSuccessStatus: 200,
     }),
 );
 app.use(router.routes());
 
-console.info("CORS-enabled web server listening on port 8000");
 await app.listen({ port: 8000 });
--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
 import { getAllUsers } from '../controllers/getAllUsers.ts';
+import { addDevice } from '../controllers/addDevice.ts';
+import { addUser } from '../controllers/addUser.ts';
 
 const router = new Router();
 
 router
-  .get("/user", getAllUsers)
+.post("/user", addUser)
+.post("/device_add", addDevice)
+.get("/users", getAllUsers)
 
 export default router;
