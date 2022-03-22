9b9a51dd403e9bd53ac5bb64b937bd405997b77e
Bertalan Lovas <lovas>
2022-03-22 11:16:04 +0100

    login backend Beta

--- backend/controllers/login.ts
+++ backend/controllers/login.ts
@@ -1,8 +1,24 @@
-import { getAllUsers } from './getAllUsers.ts';
+import { search } from './userController.ts';
 
-export async function login(){
-    const res = await fetch('http://localhost/users');
-    const data = await res.json();
+export async function login({ request, response }: { request: any; response: any }) {
+    const body = await request.body();
+    console.log({ body });
+    let status = 400;
+
+    const getUsers = await search(1);
+    const users = getUsers.rows;
     
-    console.log({data})
+
+
+    const userName = body.get('userName');
+    const password = body.get('password');
+  
+    console.log({userName, password})
+
+    //GET THE PARAMS :(
+    //COMPARE username and password
+
+
+    status = 200;
+    response.status = status;
 }
--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "https://deno.land/x/oak/mod.ts";
+
 import { getAllUsers } from '../controllers/getAllUsers.ts';
 import { addDevice } from '../controllers/addDevice.ts';
 import { addUser } from '../controllers/addUser.ts';
+import { login } from '../controllers/login.ts';
 
 const router = new Router();
 
@@ -9,5 +11,6 @@ router
 .post("/user", addUser)
 .post("/device", addDevice)
 .get("/users", getAllUsers)
+.get("/login", login)
 
 export default router;
