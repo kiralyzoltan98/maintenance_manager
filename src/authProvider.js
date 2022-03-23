609d66438dc941c11df8db5468c9f941100cc8e8
kiralyzoltan98 <kiralyzoltan98@gmail.com>
2022-03-23 20:49:52 +0100

    login front end test

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,8 +1,29 @@
 
+import { fetchUtils, Admin, Resource } from 'react-admin';
+const httpClient = fetchUtils.fetchJson;
+
 export default {
     //Called when the user tries to log in
-    login: ({username}) => {
-        localStorage.setItem('username', username);
+    login: ({username, password}) => {
+        const url = "http://localhost:8000/login";
+        let qualification;
+
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ userName: username, password: password })
+        };
+
+        async function checkLogin() {
+            const request = await httpClient(url, requestOptions);
+            const data = request.json;
+            return data;
+        }
+
+        const response = checkLogin();
+        console.log(response);
+
+        localStorage.setItem('username', response.username, 'qualification', response.qualification);
         //Accept all username/password combination
         return Promise.resolve();
     },
