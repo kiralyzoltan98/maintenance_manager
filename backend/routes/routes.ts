b3df2ef38acaccc251096ef80d99dad31c8f9303
Bertalan Lovas <lovas>
2022-04-24 22:06:54 +0200

    ready for testing

--- backend/routes/routes.ts
+++ backend/routes/routes.ts
@@ -19,6 +19,7 @@ import { addQualificationToUser } from '../controllers/addQualificationToUser.ts
 import { addTask } from '../controllers/addTask.ts';
 import { getAllMaintenances } from '../controllers/getAllMaintenances.ts';
 import { getAllTasks } from '../controllers/getAllTasks.ts';
+import { addPeriodicTask } from '../controllers/addPeriodicTask.ts';
 
 const router = new Router();
 
@@ -32,6 +33,7 @@ router
     .post("/qualification-to-category", addQualificationToCategory)
     .post("/qualification-to-user", addQualificationToUser)
     .post("/task", addTask)
+    .post("/periodic-task", addPeriodicTask)
 
     .get("/devices", getAllDevice)
     .get("/users", getAllUsers)
